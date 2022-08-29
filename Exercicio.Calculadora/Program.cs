7b4fba94220b2169050a2076272664af3533621b
Juliano Ribeiro de Souza Maciel <juliano.maciel@faculdadefacec.edu.br>
2022-08-29 19:55:30 -0300

    Implementação exercicio calculadora

---
+++ Exercicio.Calculadora/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Exercicio.Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //tenta executar código
+            try
+            {
+                #region codigo aqui
+
+                Console.WriteLine("Insira um valor:");
+                var valorDigitadoUm = Console.ReadLine();
+                decimal valorConvertidoUm = decimal.Parse(valorDigitadoUm);
+
+                Console.WriteLine("Insira outro valor:");
+                var valorDigitadoDois = Console.ReadLine();
+                decimal valorConvertidoDois = decimal.Parse(valorDigitadoDois);
+
+                // interpolacao
+                //Console.WriteLine($"Resultado Soma: {valorConvertidoUm + valorConvertidoDois}");
+
+                //concatenacao
+                Console.WriteLine("Resultado Soma: "
+                    + (valorConvertidoUm + valorConvertidoDois));
+
+                Console.WriteLine("Resultado Subtração: "
+                    + (valorConvertidoUm - valorConvertidoDois));
+
+                Console.WriteLine("Resultado Multiplicação: "
+                    + (valorConvertidoUm * valorConvertidoDois));
+
+                Console.WriteLine("Resultado Divisão: "
+                    + (valorConvertidoUm / valorConvertidoDois));
+
+                Console.WriteLine("Resultado Resto Divisão: "
+                    + (valorConvertidoUm % valorConvertidoDois));
+
+                #endregion codigo aqui
+            }
+            //captura erro/falha/excessao
+            catch 
+            {
+                Console.WriteLine("Digite um valor correto!");
+            }
+            //finaliza o sistema
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
